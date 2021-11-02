3e52f8a553f7070f9c6c331157d9d8a01c9fa4f2
Xuanxuan137 <qianben5@icloud.com>
2021-11-02 19:38:31 +0800

    create preprocess file

---
+++ util/preprocess.cpp
@@ -0,0 +1,5 @@
+//
+// Created by noname on 2021/11/2.
+//
+
+#include "preprocess.h"
---
+++ util/preprocess.h
@@ -0,0 +1,16 @@
+//
+// Created by noname on 2021/11/2.
+//
+
+#ifndef QUANT_PREPROCESS_H
+#define QUANT_PREPROCESS_H
+
+
+
+class preprocess {
+
+};
+
+
+
+#endif //QUANT_PREPROCESS_H
