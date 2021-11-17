cc178dce405914c4b710e286f5c1a80c131b80e4
Xuanxuan137 <qianben5@icloud.com>
2021-11-17 19:59:40 +0800

    test git

--- nn/graph.cpp
+++ nn/graph.cpp
@@ -538,6 +538,7 @@ Graph *Graph::quantization(Tensor<uint8>* calib_set, Tensor<float32>* processed_
     // 6. 对需要的层的weight和bias进行量化
     for(int i = 0; i<node_number; i++) {
         printf("Hello world\n");
+        printf("world hello\n");
     }
     // 7. 将s, z, m0, n存入量化计算图的各个算子之中
 
